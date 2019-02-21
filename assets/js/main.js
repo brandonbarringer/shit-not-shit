fb48b1802f3a1d5c791462ac6588d3cab3548d47
Brandon Barringer <brandon.barringer@CS4-BrandonBarringer-iMac.local>
2019-02-20 20:40:02 -0800

    [init] adds basic html and animation states

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+  var rippleWrap = $('.ripple-wrap');
+  var rippler = $('.ripple');
+  var finish = false;
+  var monitor = function(el) {
+    var computed = window.getComputedStyle(el, null);
+    var borderwidth = parseFloat(
+      computed.getPropertyValue('border-left-width')
+    );
+    if (!finish && borderwidth >= 1500) {
+      el.style.WebkitAnimationPlayState = 'paused';
+      el.style.animationPlayState = 'paused';
+      swapContent();
+    } 
+    if (finish) {
+      el.style.WebkitAnimationPlayState = 'running';
+      el.style.animationPlayState = 'running';
+      return;
+    } else {
+      window.requestAnimationFrame(function() {
+        monitor(el);
+      });
+    }
+  };
+
+  var storedContent = $('.content--second').html();
+  $('.content--second').remove();
+
+  rippler.bind('webkitAnimationEnd oAnimationEnd msAnimationEnd mozAnimationEnd animationend', function() {
+    rippleWrap.removeClass('goripple');
+  });
+
+  $('body').on('click', '.transition', function(e) {
+    rippler.css('left', e.clientX + 'px');
+    rippler.css('top', e.clientY + 'px');
+    e.preventDefault();
+    finish = false;
+    rippleWrap.addClass('goripple');
+    window.requestAnimationFrame(function() {
+      monitor(rippler[0]);
+    });
+  });
+
+  function swapContent() {
+    var newContent = $('.content--first').html();
+    $('.content--first').html(storedContent);
+    storedContent = newContent;
+    // do some Ajax, put it in the DOM and then set this to true
+    setTimeout(function() {
+      finish = true;
+    }, 10);
+  }
+});
